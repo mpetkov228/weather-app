ff5fcba962134f722c464ceaad0f713ade0379ce
MihailPetkov96 <mpetkov228@gmail.com>
2023-11-21 18:44:09 +0200

    refactor getCurrentWeather function

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const API_KEY = '79d69622fa02417c8a8160044232011';
 
-async function getCurrentWeather() {
-    const location = 'Burgas';
+const location = document.getElementById('location');
+
+async function getCurrentWeatherData(location) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    return data;
 }
 
-getCurrentWeather();
