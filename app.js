5a0e232662a7a80687f1fcc2fe14d4f63b7f88ae
MihailPetkov96 <mpetkov228@gmail.com>
2023-11-21 18:50:54 +0200

    Add input bar for custom location

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const API_KEY = '79d69622fa02417c8a8160044232011';
 
-const location = document.getElementById('location');
+const city = document.getElementById('location');
+
+city.addEventListener('keyup', async (event) => {
+    if (event.key == 'Enter') {
+        let data = await getCurrentWeatherData(event.target.value);
+        console.log(data);
+    }
+});
 
 async function getCurrentWeatherData(location) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
